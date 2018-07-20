4fd750a1028f45896d9edb43be1816a2092bdbc6
Megan Prior-Pfeifer <mprior@apple.com>
2018-07-20 16:08:30 -0700

    feat: basic script to render the fields

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
+const options = ['Trump', 'Bad Service', 'Farts'];
+
+const storeText = 'Store This'
+const storeBtn = <button id="store">{storeText}</button>
+
+const clearText = 'Clear Fields'
+const clearBtn = <button id="clear">{clearText}</button>
+
+/**
+ * Represents the categories that have been created by the user.
+ * There is no limit.
+ */
+function CategoryOption(props) {
+	const optionItems = options.map((opt, index) =>
+		<option key={index} value={index}>{opt}</option>
+	);
+
+	return (
+		<select name="category" id="category">
+			<option value="0" disabled>Choose a topic</option>
+			{optionItems}
+		</select>
+	);
+}
+
+/**
+ * Represents the inputs to create a new note.
+ * There is no limit.
+ */
+function NoteInput(props) {
+	return (
+		<div id="note-input" class="note-input">
+			<input type="text" placeholder="Write a snazzy title" id="titleInput" value="Title"/>
+			<CategoryOption />
+			<textarea placeholder="Type some stuff for your note" id="contentInput">Text</textarea>
+			{storeBtn}{clearBtn}
+		</div>
+	);
+}
+
+/**
+ * Renders the app
+ */
 class App extends Component {
   render() {
 	return (
 		<div class="app">
-			<div id="note-input" class="note-input">
-			  <input type="text" placeholder="Write a snazzy title" id="titleInput" value="Title"/>
-			  <select name="category" id="category">
-				<option value="0" disabled>Choose a topic</option>
-				<option value="1" selected>Trump</option>
-				<option value="2">Bad Service</option>
-				<option value="3">Farts</option>
-			  </select>
-			  <textarea placeholder="Type some stuff for your note" id="contentInput">Text</textarea>
-			  <button id="store">Store This</button> <button id="clear">Clear Fields</button>
-			</div>
+			<NoteInput />
 			<div id="notes"></div>
 		</div>
 	);
